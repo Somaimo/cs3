e92db11c31b2a4b2c5b3f179e7369f5ea453c2c7
Somaimo <camao@bluewin.ch>
2019-10-23 22:53:51 +0200

    add owncloud model and add to admin backend

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,22 @@
+# pages/admin.py
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import Installation
+
+# We override the default Installation Model view to set readonly fields (user and deleted flag) and that
+# the current user is stored as "user" for the installation.
+class InstallationAdmin(admin.ModelAdmin):
+    # Defines what fields are read only in the admin panel (only for Installation Model)
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = ['user','deleted',]
+        return readonly_fields
+    
+    # Function override required to save current user on first save.
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            # Only save user during first save
+            obj.user = request.user
+        super().save_model(request, obj, form, change)
+admin.site.register(Installation, InstallationAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,14 @@
 from django.views.generic import TemplateView
-
+from .api_keys import ApiKeys
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['maps_key'] = ApiKeys.google_maps_key
+        return context
+
 
 
 class AboutPageView(TemplateView):
